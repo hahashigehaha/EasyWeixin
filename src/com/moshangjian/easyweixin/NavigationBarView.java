4a7272c0b536aa5711fe3e400875bb1e0592e046
hushuaibing <446810035@qq.com>
2015-06-05 23:11:39 +0800

    final version

--- src/com/moshangjian/easyweixin/NavigationBarView.java
+++ src/com/moshangjian/easyweixin/NavigationBarView.java
@@ -1,6 +1,8 @@
 package com.moshangjian.easyweixin;
 
 import android.content.Context;
+
+import android.view.View.OnClickListener; 
 import android.graphics.Color;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
@@ -10,7 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 
-public class NavigationBarView extends LinearLayout {
+public class NavigationBarView extends LinearLayout implements OnClickListener {
 
 	public NavigationBarView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
@@ -32,7 +34,6 @@ public class NavigationBarView extends LinearLayout {
 		setBackgroundColor(Color.parseColor("#fcfcfc"));
 		LayoutInflater inflater = LayoutInflater.from(context);
 		inflater.inflate(R.layout.navigation_bar, this);
-		
 	}
 	
 	
@@ -43,27 +44,40 @@ public class NavigationBarView extends LinearLayout {
 		two = (BottomSingleView) findViewById(R.id.nav_two);
 		three = (BottomSingleView) findViewById(R.id.nav_three);
 		four = (BottomSingleView) findViewById(R.id.nav_four);
+		
+		
+		one.setOnClickListener(this);
+		two.setOnClickListener(this);
+		three.setOnClickListener(this);
+		four.setOnClickListener(this);
 	}
 	
+	private ViewPager mViewPager ; 
 	public void setViewPager(ViewPager mViewPager){
-		mViewPager.setOnPageChangeListener(listener);
+		this.mViewPager = mViewPager ; 
+		this.mViewPager.setOnPageChangeListener(listener);
 	}
 
 	
    private OnPageChangeListener listener = new OnPageChangeListener(){
 	   @Override
 	   public void onPageScrollStateChanged(int arg0) {
+		   if (arg0 == 0 ) {
+			   isClick = false ; 
+		}
 	   }
 	   @Override
 	   public void onPageScrolled(int position, float positionOffset, int arg2) {
-		   setChildProgress(position, 1 - positionOffset);
-		   setChildProgress(position + 1, positionOffset);
+		   if (!isClick) {
+			   setChildProgress(position, 1 - positionOffset);
+			   setChildProgress(position + 1, positionOffset);
+		   }
 	   }
 	   @Override
 	   public void onPageSelected(int arg0) {
+		   currentID = arg0  ; 
 	   }
   };
-  
   
   
   private  void setChildProgress(int position , float progress){
@@ -73,7 +87,7 @@ public class NavigationBarView extends LinearLayout {
 			break;
 		case 1:
 			two.setProgress(progress);
-				break;
+			break;
 		case 2 :
 			three.setProgress(progress);
 			break;
@@ -82,8 +96,58 @@ public class NavigationBarView extends LinearLayout {
 			break;
 	}
   }
-private BottomSingleView one;
-private BottomSingleView two;
-private BottomSingleView three;
-private BottomSingleView four;
+  private BottomSingleView one;
+  private BottomSingleView two;
+	private BottomSingleView three;
+	private BottomSingleView four;
+
+	private int currentID = 0 ; 
+
+	
+	private boolean isClick = false ; 
+	@Override
+	public void onClick(View v) {
+		isClick = true ; 
+		restoreState(currentID);
+		switch (v.getId()) {
+		case R.id.nav_one:
+			one.setProgress(1);
+			currentID = 0 ;
+			mViewPager.setCurrentItem(0);
+			break;
+		case R.id.nav_two:
+			two.setProgress(1);
+			currentID = 1 ; 
+			mViewPager.setCurrentItem(1);
+			break;
+		case R.id.nav_three:
+			three.setProgress(1);
+			currentID = 2 ;
+			mViewPager.setCurrentItem(2);
+			break;
+		case R.id.nav_four:
+			four.setProgress(1);
+			currentID = 3 ;
+			mViewPager.setCurrentItem(3);
+			break ; 
+		}
+	}
+	
+	private void restoreState(int lastPosition){
+		switch (lastPosition) {
+			case 0:
+				one.setProgress(0);
+				break;
+			case 1:
+				two.setProgress(0);
+				break;
+			case 2:
+				three.setProgress(0);
+				break;
+			case 3:
+				four.setProgress(0);
+				break;
+		}
+	}
+	
 }
